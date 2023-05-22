eb284ab87a6d1ca30d26fcf30db7761162e14125
sonia jaiswal <118751361+soniaj27@users.noreply.github.com>
2023-05-22 09:14:55 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Animal {}
 
-class Dog extends Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Cat extends Animal {}
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log(`purr`);
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log(`woof`);
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
